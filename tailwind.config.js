9079d30e109034a329bbbb9f300e2d02842cef99
nurulla-hasan <nurullahasan.dev@gmail.com>
2026-06-27 15:46:28 +0600

    Add config files (vite, ts, eslint, tailwind, postcss), fix authSlice module import error

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#f7f7f7',
+        'btnPrimary': '#d1ff43',
+      },
+      fontFamily: {
+        inter: ['"Inter"', 'system-ui', 'sans-serif'],
+        familjen: ['"Familjen Grotesk"', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
